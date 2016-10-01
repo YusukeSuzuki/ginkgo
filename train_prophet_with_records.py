f9ee0619ba5cb702cf574a033c426ac54f55b7b7
Yusuke Suzuki <yusuke@geekfield.jp>
2016-10-02 00:48:58 +0900

    working on train_prophet_with_records.py

--- train_prophet_with_records.py
+++ train_prophet_with_records.py
@@ -2,3 +2,75 @@ import tensorflow as tf
 import prophet_model
 
 
+# ------------------------------------------------------------
+# sub commands
+# ------------------------------------------------------------
+
+def do_train(namespace):
+    models_dir = Path(MODELS_DIR)
+    models_dir.mkdir(parents=True, exist_ok=True)
+    model_path = models_dir/namespace.modelfile
+    model_backup_path = models_dir/(namespace.modelfile+'.back')
+
+def do_test(namespace):
+    print('unavailable now')
+
+def do_eval(namespace):
+    print('unavailable now')
+
+def do_dump_network(namespace):
+    pass
+
+def do_dump_graph_log(namespace):
+    pass
+
+# ------------------------------------------------------------
+# command line option parser
+# ------------------------------------------------------------
+
+def create_parser():
+    parser = AP(prog='train_prophet_with_records')
+    parser.set_defaults(func=None)
+    parser.add_argument('--logdir', type=str, default='./logs')
+    parser.add_argument('--modelfile', type=str, default='model.ckpt')
+    parser.add_argument('--restore', type=str, default='')
+    sub_parsers = parser.add_subparsers()
+
+    sub_parser = sub_parsers.add_parser('train')
+    sub_parser.set_defaults(func=do_train)
+    sub_parser.add_argument('--optimizer', type=str, required=True)
+    sub_parser.add_argument('--samples', type=str, default='./samples')
+    sub_parser.add_argument('--trainable-scope', type=str, default='')
+
+    sub_parser = sub_parsers.add_parser('test')
+    sub_parser.set_defaults(func=do_test)
+
+    sub_parser = sub_parsers.add_parser('eval')
+    sub_parser.set_defaults(func=do_eval)
+
+    sub_parser = sub_parsers.add_parser('dump-network')
+    sub_parser.set_defaults(func=do_dump_network)
+    sub_parser.add_argument('--pattern', type=str, default='*')
+
+    sub_parser = sub_parsers.add_parser('dump-graph-log')
+    sub_parser.set_defaults(func=do_dump_graph_log)
+    sub_parser.add_argument('--exclude-tags', type=str, default='')
+
+    return parser
+
+# ------------------------------------------------------------
+# main
+# ------------------------------------------------------------
+
+def run():
+    parser = create_parser()
+    namespace = parser.parse_args()
+
+    if namespace.func:
+        namespace.func(namespace)
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    run()
+
