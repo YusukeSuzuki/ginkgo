23eb23a079cb40fdb49d4ab54a6a2540b8bceb3d
Yusuke Suzuki <yusuke@geekfield.jp>
2016-10-08 15:33:36 +0900

    add batch training

--- train_prophet_with_records.py
+++ train_prophet_with_records.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser as AP
 from pathlib import Path
 import fnmatch
-from random import choice
+from random import choice, randrange, shuffle
 import math
+import sys
 
 import tensorflow as tf
 import numpy as np
@@ -16,6 +17,8 @@ import shogi_records
 ROOT_VARIABLE_SCOPE='prophet'
 MODEL_YAML_PATH = 'prophet_model.yaml'
 MODELS_DIR = 'models'
+
+MINI_BATCH_SIZE = 20
 
 # ------------------------------------------------------------
 # sub commands
@@ -32,7 +35,7 @@ def do_train(namespace):
 
     # build
     with tf.variable_scope('input'), tf.device('/cpu:0'):
-        input_vector = tf.placeholder(tf.float32, [1,9,9,148])
+        input_vector = tf.placeholder(tf.float32, [MINI_BATCH_SIZE,9,9,148])
 
     with tf.variable_scope(ROOT_VARIABLE_SCOPE):
         graph_root = yl.load(MODEL_YAML_PATH)
@@ -70,7 +73,7 @@ def do_train(namespace):
 
     writer.add_graph(tf.get_default_graph())
 
-    print('train')
+    print('convert records')
 
     #print(tags)
     #print(train)
@@ -78,16 +81,24 @@ def do_train(namespace):
 
     score = []
 
-    for i in range(0, 500000):
-        while True:
-            record = choice(records)
-            sfen, side, turn, total, move, winner = shogi_records.to_data(record)
-            if side != 'b':
-                continue
-            if int(turn) < 16:
-                continue
+    shuffle(records)
+    new_records = []
 
-            break
+    for r in records:
+        sfen, side, turn, total, move, winner = shogi_records.to_data(r)
+        if side != 'b':
+            continue
+        if int(turn) < 20:
+            continue
+        new_records.append( (sfen, side, turn, total, move, winner) )
+
+    records = new_records
+    vecs = []
+    
+    print('create vecs')
+
+    for r in records:
+        sfen, side, turn, total, move, winner = r
 
         board = shogi.Board(sfen)
         board.push_usi(move)
@@ -97,26 +108,63 @@ def do_train(namespace):
             1.0 if side == winner else 0.0,
             1.0 if side != winner else 0.0])
 
+        vecs.append( (np.squeeze(board_vec, axis=0), match_vec, [math.sqrt(float(turn)/float(total))]) )
+        
+        if len(vecs) % 10 == 0:
+            print(len(vecs))
+
+
+    vecs_iter = iter(vecs)
+
+
+    def create_batch():
+        board_list = []
+        match_list = []
+        weight_list = []
+        print('choice start')
+
+        for i in range(MINI_BATCH_SIZE):
+            b,m,w = vecs_iter.__next__()
+
+            board_list.append(b)
+            match_list.append(m)
+            weight_list.append(w)
+
+        board_batch = np.stack( tuple(board_list) )
+        match_batch = np.stack( tuple(match_list) )
+        weight_batch = np.stack( tuple(weight_list) )
+
+        print('choice end')
+        return board_batch, match_batch, weight_batch
+
+    print('train')
+
+    for i in range(0, 300000):
+        board_batch, match_batch, weight_batch = create_batch()
+
         if (i + 1) % 10 == 0:
             summary, out, res = sess.run( (merged, tags['out'], train),
-                feed_dict={ input_vector: board_vec, tags['label']: match_vec,
-                    tags['turn_weight']: [math.sqrt(float(turn)/float(total))]} )
+                feed_dict={ input_vector: board_batch, tags['label']: match_batch,
+                    tags['turn_weight']: weight_batch} )
             writer.add_summary(summary, i)
         else:
             out, res = sess.run( (tags['out'], train),
-                feed_dict={ input_vector: board_vec, tags['label']: match_vec,
-                    tags['turn_weight']: [math.sqrt(float(turn)/float(total))]} )
+                feed_dict={ input_vector: board_batch, tags['label']: match_batch,
+                    tags['turn_weight']: weight_batch} )
 
-        ret_winner = 'b' if out[0][0] > out[0][1] else 'w'
-        score.append(1 if ret_winner == winner else 0)
+        if False:
+            ret_winner = 'b' if out[0][0] > out[0][1] else 'w'
+            score.append(1 if ret_winner == winner else 0)
 
-        print('loop: {}, {}, {}, {}, {}'.format(i, out,
-            '<' if out[0][0] > out[0][1] else '>',
-            '<' if match_vec[0] < match_vec[1] else '>',
-            sum(score) / len(score)))
+            print('loop: {}, {}, {}, {}, {}'.format(i, out,
+                '<' if out[0][0] > out[0][1] else '>',
+                '<' if match_vec[0] < match_vec[1] else '>',
+                sum(score) / len(score)))
 
-        if len(score) == 200:
-            score.pop(0)
+            if len(score) == 200:
+                score.pop(0)
+        else:
+            print('loop: {}'.format(i))
 
         if i > 10 and i % 4000 == 1:
             print('save backup to: {}'.format(model_backup_path))
