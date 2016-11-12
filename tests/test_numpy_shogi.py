4308aecf98ad4801baaa9debb0ff71c9ef8bd47d
Yusuke Suzuki <yusuke@geekfield.jp>
2016-11-12 19:04:33 +0900

    add unittests stub files

---
+++ tests/test_numpy_shogi.py
@@ -0,0 +1,23 @@
+import unittest as ut
+import ginkgo.numpy_shogi as ns
+
+class NumpyShogiTest(ut.TestCase):
+    CLS_VAL='none'
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    ut.main()
+
